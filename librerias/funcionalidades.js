04382ea19c2599e2cb790acfb9ab6d648707705d
iñaki-home <ikitess@gmail.com>
2020-08-27 23:02:57 +0200

    build 0.01

--- librerias/funcionalidades.js
+++ librerias/funcionalidades.js
@@ -1,25 +1,25 @@
 $(document).ready(function () {
 
-    // POR DEFECTO PONER QUE SE CARGUEN LOS PROYECTOS
+    // POR DEFECTO PONER QUE SE CARGUEN LOS PROYECTOS o info
 
-         // it works!!!!
-        let result= null;
-        $.ajax({
-            url: "https://api.github.com/users/inakicl/repos?type=owner",
-            async: false,
-        }).done(function (data) {
-            result = data;
-        });
-    
-    
-    
-        console.log(result[0]); 
+    // it works!!!!
+    let result = null;
+    $.ajax({
+        url: "https://api.github.com/users/inakicl/repos?type=owner",
+        async: false,
+    }).done(function (data) {
+        result = data;
+    });
+
+
+
+    console.log(result[0]);
 
     // si existe el div pa porsi.
     let divInfos = null;
     if ($('#infos').length > 0) {
         divInfos = $('#infos');
-       // divInfos.append('<div class="bg-warning"> f</div>')
+        // divInfos.append('<div class="bg-warning"> f</div>')
         clickInfoPersonal();
     }
 
@@ -36,6 +36,12 @@ $(document).ready(function () {
 
     //funciones botones
     function clickInfoPersonal() {
+        divInfos.empty();
+
+        divInfos.append('' +
+            '<div class="col-12 bg-especial w-100 p-0 "> auxilio</div>');
+
+
         /*divInfos.empty();
 
         divInfos.append('<div class="bg-warning"> f</div>')
@@ -55,11 +61,11 @@ $(document).ready(function () {
     function clickProyectos() {
         divInfos.empty();
 
-        divInfos.append('<div class="bg-warning"> d</div>')
+        divInfos.append('<div class="col-12 bg-warning w-100 p-0 "> d</div>')
     }
     function clickContacto() {
         divInfos.empty();
 
-        divInfos.append('<div class="bg-warning"> ws</div>')
+        divInfos.append('<div class="col-12 bg-warning w-100 p-0 "> wsw</div>')
     }
 })
